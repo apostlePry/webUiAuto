5c848d5ab0b481fd4d5edc5272daefd5e8610582
jiaqinwen <731971603@qq.com>
2020-07-04 16:38:34 +0800

    增加app测试类，测试webview

---
+++ src/test/java/cn/study/page/app/AppWebViewTest.java
@@ -0,0 +1,57 @@
+package cn.study.page.app;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class AppWebViewTest {
+
+    protected static AppiumDriver<MobileElement> driver;
+
+    @BeforeAll
+    public static void before(){
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "android");
+        desiredCapabilities.setCapability("deviceName", "emulator-5554");
+        desiredCapabilities.setCapability("udid", "emulator-5554");
+        desiredCapabilities.setCapability("appPackage", "com.xueqiu.android");
+        desiredCapabilities.setCapability("appActivity", ".view.WelcomeActivityAlias");
+        desiredCapabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+
+        URL remoteUrl = null;
+        try {
+            remoteUrl = new URL("http://localhost:4723/wd/hub");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        driver = new AndroidDriver<>(remoteUrl, desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.quit();
+    }
+
+    @Test
+    public void testWebView(){
+        driver.findElement(By.xpath("//*[@text='交易']")).click();
+        driver.findElement(By.xpath("//*[@text='基金开户']")).click();
+    }
+}
